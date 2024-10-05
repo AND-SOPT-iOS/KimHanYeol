8c07dd39bd7058fdb0d38c92441be1cd5599a167
OneTen19 <khy000426@naver.com>
2024-10-05 14:09:23 +0900

    Initial Commit

---
+++ 35-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  35-Seminar
+//
+//  Created by OneTen on 10/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
