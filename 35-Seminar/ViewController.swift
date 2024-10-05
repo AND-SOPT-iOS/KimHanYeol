9edf9b6437c521ac27e6ae1f9dc93815ee39a08c
OneTen19 <khy000426@naver.com>
2024-10-05 21:24:04 +0900

    Initial commit

--- 35-Seminar/ViewController.swift
+++ 35-Seminar/ViewController.swift
@@ -8,12 +8,183 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 32)
+        return label
+    }()
+    
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "제목을 입력해주세요."
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private let contentTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 14)
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.layer.borderWidth = 1
+        textView.layer.cornerRadius = 5
+        return textView
+    }()
+    
+    private lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다음", for: .normal)
+        button.backgroundColor = .red
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    //전환버튼
+    private lazy var pushModeToggleButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전환 모드 변경", for: .normal)
+        button.backgroundColor = .tintColor
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(toggleButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private var pushMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setStyle()
+        setUI()
+        setLayout()
     }
-
-
+    
+    private func setStyle() {
+        self.view.backgroundColor = .white
+        updateUI()
+    }
+    
+    private func setUI() {
+        [
+            titleLabel,
+            titleTextField,
+            contentTextView,
+            nextButton,
+            pushModeToggleButton
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.topAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.topAnchor,
+                    constant: 20
+                ),
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                
+                
+                
+                titleTextField.topAnchor.constraint(
+                    equalTo: titleLabel.bottomAnchor,
+                    constant: 20
+                ),
+                titleTextField.leadingAnchor.constraint(
+                    equalTo: view.leadingAnchor,
+                    constant: 20
+                ),
+                titleTextField.trailingAnchor.constraint(
+                    equalTo: view.trailingAnchor,
+                    constant: -20
+                ),
+                titleTextField.heightAnchor.constraint(equalToConstant: 40),
+                
+                
+                
+                contentTextView.topAnchor.constraint(
+                    equalTo: titleTextField.bottomAnchor,
+                    constant: 20
+                ),
+                contentTextView.leadingAnchor.constraint(
+                    equalTo: view.leadingAnchor,
+                    constant: 20
+                ),
+                contentTextView.trailingAnchor.constraint(
+                    equalTo: view.trailingAnchor,
+                    constant: -20
+                ),
+                contentTextView.heightAnchor.constraint(
+                    equalToConstant: 200
+                ),
+                
+                
+                
+                
+                nextButton.bottomAnchor.constraint(
+                    equalTo: pushModeToggleButton.topAnchor,
+                    constant: -20
+                ),
+                nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                nextButton.heightAnchor.constraint(equalToConstant: 44),
+                nextButton.widthAnchor.constraint(equalToConstant: 300),
+                
+                
+                
+                pushModeToggleButton.bottomAnchor.constraint(
+                    equalTo: view.bottomAnchor,
+                    constant: -40
+                ),
+                pushModeToggleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                pushModeToggleButton.heightAnchor.constraint(equalToConstant: 44),
+                pushModeToggleButton.widthAnchor.constraint(equalToConstant: 300),
+                
+                
+            ]
+        )
+    }
+    
+    private func updateUI() {
+        self.titleLabel.text = pushMode ? "네비게이션" : "모달"
+    }
+    
+    @objc func nextButtonTapped() {
+        transitionToNextViewController()
+    }
+    
+    private func transitionToNextViewController() {
+        let nextViewController = DetailViewController()
+        guard let title = titleTextField.text,
+              let content = contentTextView.text
+        else {
+            // 텍스트의 값들이 없으면 함수가 return
+            return
+        }
+        // 존재할 경우 함수를 그대로 실행
+        nextViewController.dataBind(
+            title: title,
+            content: content
+        )
+        if pushMode {
+            self.navigationController?.pushViewController(
+                nextViewController,
+                animated: true
+            )
+        } else {
+            self.present(
+                nextViewController,
+                animated: true
+            )
+        }
+    }
+    
+    @objc func toggleButtonTapped() {
+        self.pushMode.toggle()
+        self.updateUI()
+    }
 }
-
