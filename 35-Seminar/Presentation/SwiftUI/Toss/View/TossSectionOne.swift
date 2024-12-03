0eed93270c002a53445bf80b22f688f53b21fc06
OneTen19 <khy000426@naver.com>
2024-12-03 20:41:24 +0900

    Feat/#17 - 토스 뷰 섹션별 파일 나누기1

---
+++ 35-Seminar/Presentation/SwiftUI/Toss/View/TossSectionOne.swift
@@ -0,0 +1,59 @@
+//
+//  TossSectionOne.swift
+//  35-Seminar
+//
+//  Created by OneTen on 12/3/24.
+//
+
+import SwiftUI
+
+struct TossSectionOne: View {
+    var body: some View {
+        HStack(alignment: .bottom){
+            Image(.tossApp)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 130)
+                .clipShape(.buttonBorder)
+                .shadow(radius: 1)
+            
+            
+            VStack(alignment: .leading){
+                Text("토스")
+                    .font(.system(size: 26, weight: .semibold))
+                
+                Text("금융이 쉬워진다")
+                    .font(.system(size: 16))
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Text("받기")
+                        .font(.system(size: 20, weight: .semibold))
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 25)
+                        .padding(.vertical, 5)
+                        .background(.blue)
+                        .clipShape(.capsule)
+                }
+                
+            }
+            .padding(.horizontal, 10)
+            
+            Spacer()
+            
+            Image(systemName: "square.and.arrow.up")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20)
+                .foregroundStyle(.blue)
+                .padding(.bottom, 5)
+        }
+    }
+}
+#Preview {
+    TossSectionOne()
+}
--- 35-Seminar/Presentation/SwiftUI/Toss/View/TossView.swift
+++ 35-Seminar/Presentation/SwiftUI/Toss/View/TossView.swift
@@ -23,53 +23,7 @@ struct TossView: View {
     }
 }
 
-struct TossSectionOne: View {
-    var body: some View {
-        HStack(alignment: .bottom){
-            Image(.tossApp)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 130)
-                .clipShape(.buttonBorder)
-                .shadow(radius: 1)
-            
-            
-            VStack(alignment: .leading){
-                Text("토스")
-                    .font(.system(size: 26, weight: .semibold))
-                
-                Text("금융이 쉬워진다")
-                    .font(.system(size: 16))
-                    .foregroundStyle(.gray)
-                
-                Spacer()
-                
-                Button {
-                    //
-                } label: {
-                    Text("받기")
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundStyle(.white)
-                        .padding(.horizontal, 25)
-                        .padding(.vertical, 5)
-                        .background(.blue)
-                        .clipShape(.capsule)
-                }
-                
-            }
-            .padding(.horizontal, 10)
-            
-            Spacer()
-            
-            Image(systemName: "square.and.arrow.up")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 20)
-                .foregroundStyle(.blue)
-                .padding(.bottom, 5)
-        }
-    }
-}
+
 
 struct TossSectionTwo: View {
     var body: some View {
